6b6b1c4c7debd15cba4a09e080ae98f3a1b754fb
Steven Love <steven@terel.io>
2024-10-22 09:02:54 -0700

    modified to showcase message passing

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# Development
+# Purpose
+This is an example project to explore Dioxus's message passing between Rust and javascript using `eval`, `dioxus.send` and `dioxus.recv`
 
-Run the following command in the root of the project to start the Dioxus dev server:
+# Procedure
+`cargo run`
+tap the `Send timestamps in a loop` button
+watch as local_timestamps and remote_timestamps fill with timestamps as a message is passed from Rust->js->Rust
+eventually, within 1 minute, it breaks. A call to `eval.recv().await` returns an `Err(Finished)` which you can see in the CLI logs.
 
-```bash
-dx serve --hot-reload --platform desktop
-```
+# Questions
+- Is there a better practice with Dioxus that will allow me to make RPC-style calls to javascript, passing in some args and getting back a return value
+- Is there a problem with using `eval` repeatedly (100s of times) in a loop?
+- Is this a timing issue that could be resolved by adding sleeps or timeouts?
+
+# Version
+Dioxus 0.5.6
+not using `dx` Dioxus CLI
--- src/main.rs
+++ src/main.rs
@@ -37,18 +37,46 @@ fn Blog(id: i32) -> Element {
 #[component]
 fn Home() -> Element {
     let mut count = use_signal(|| 0);
+    let mut local_timestamps = use_signal::<Vec<u128>>(|| vec![]);
+    let mut remote_timestamps = use_signal::<Vec<u128>>(|| vec![]);
 
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
+        Link { to: Route::Blog { id: count() }, "Go to blog" }
         div {
             h1 { "High-Five counter: {count}" }
             button { onclick: move |_| count += 1, "Up high!" }
             button { onclick: move |_| count -= 1, "Down low!" }
+            button {
+                onclick: move |_| async move {
+                    loop {
+                        let t: u128 = std::time::SystemTime::now()
+                            .duration_since(std::time::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis();
+                        local_timestamps.push(t);
+                        let mut eval = eval(
+                            &format!(
+                                r#"
+                    let t = {t};
+                    console.log("local timestamp",t);
+                    dioxus.send(t);
+                    "#,
+                            ),
+                        );
+                        let received = eval.recv().await;
+                        let Ok(received) = received else {
+                            println!("Failure to recv! {:?}", received);
+                            return;
+                        };
+                        let parsed = serde_json::from_value::<u128>(received).unwrap();
+                        remote_timestamps.push(parsed);
+                    }
+                },
+                "Send timestamps in a loop"
+            }
         }
+
+        div { {format!("local_timestamps: {:?}",local_timestamps)  } }
+        div { {format!("remote_timestamps: {:?}", remote_timestamps)  } }
     }
 }
