49d41fae5c06242cceaf18f352cfb6b750e938dd
Steven Love <steven@terel.io>
2024-12-24 21:09:57 -0500

    toast

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
 # Purpose
-This is an example project to explore Dioxus's window management
+This repo demonstrates problematic behavior with the dioxus_toast crate.  
+When run in a `tokio::task::LocalSet` task, the application hangs after 15-20 seconds.
 
 # Procedure
 `cargo run`
-The app should open two windows, one that says "Hello" and one that says "World"
-Tap the operating system's [x] button to close a window, and then tap the other [x] to close the other window.
-
-In dioxus 0.5, this works just fine and you can close either window first.
-<video src='./0.5-example.mov' width=300>
-
-
-In dioxus 0.6.0, the windows don't react to the [x] button until you have attempted to close each both of the windows. 
-
-<video src='./0.6-example.mov' width=300>
-
-# Attempted workaround
-I wanted to try using `use_wry_event_handler` to manually handle the `WindowEvent::CloseRequested` event, but I am struggling to hear any events on windows other than the first one (the one labeled "Hello" in this case). Right now the demo app tracks `WindowEvent::Moved` and prints to the console. You can see that moving the "Hello" window prints events and no events are printed when you move the other window.
+The app should open a window that counts up and displays toasts. It typically hangs after 13 seconds when it is displaying `26`.
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,67 @@
-//! The typical TodoMVC app, implemented in Dioxus.
+use std::time::Duration;
 
-use dioxus::desktop::tao::event::{Event, WindowEvent};
-use dioxus::desktop::use_wry_event_handler;
-use dioxus::prelude::*;
+use dioxus::{
+    desktop::{launch::launch_virtual_dom_blocking, Config},
+    prelude::*,
+};
+use dioxus_toast::{ToastInfo, ToastManager};
+use tokio::task::{spawn_local, LocalSet};
 
-fn main() {
-    dioxus::launch(App);
+#[derive(Clone, Copy)]
+struct DioxusMainBridgeContext {
+    rsx_signal: Signal<Result<VNode, RenderError>>,
+}
+
+/// Run a local task that updates an rsx signal asynchronously
+async fn contentful_main(mut ctx: DioxusMainBridgeContext) {
+    let mut toast = use_signal(|| ToastManager::default());
+
+    spawn_local(async move {
+        let mut i = 0;
+        loop {
+            let my_rsx = rsx! {
+                dioxus_toast::ToastFrame { manager: toast }
+                h1 { {format!("{}", i)} }
+            };
+            i += 1;
+            ctx.rsx_signal.set(my_rsx);
+            let _id = toast
+                .write()
+                .popup(dioxus_toast::ToastInfo::simple(i.to_string().as_str()));
+            tokio::time::sleep(Duration::from_millis(500)).await;
+        }
+    });
 }
 
 #[component]
 pub fn App() -> Element {
     rsx! {
-        RecursiveComponent { contents: vec!["Hello".to_string(), "World".to_string()] }
+        h1 { "placeholder" }
     }
 }
 
-#[component]
-pub fn RecursiveComponent(contents: Vec<String>) -> Element {
-    use_wry_event_handler(move |e, _| {
-        if let Event::WindowEvent {
-            event: WindowEvent::Moved(_),
-            window_id,
-            ..
-        } = e
-        {
-            // if window_id != &window_id_signal.cloned().unwrap() {
-            //     println!("matching window ids");
-            //     window_signal.cloned().unwrap().close_window(window_id.clone());
-            //     return;
-            // }
-            println!("event {:?} {:?}", e, window_id);
+#[tokio::main]
+async fn main() {
+    let config = Config::default();
+    let virtual_dom = VirtualDom::new(|| {
+        let rsx_signal = use_signal(|| {
+            rsx! {
+                h1 { "placeholder" }
+            }
+        });
+        let ctx = DioxusMainBridgeContext { rsx_signal };
+
+        // Wrap our contentful_main in a LocalSet so we can use async with !Send types
+        async fn local_set_wrapper(ctx: DioxusMainBridgeContext) {
+            let executor = LocalSet::new();
+            executor.spawn_local(contentful_main(ctx));
+            executor.await;
         }
+
+        use_future(move || local_set_wrapper(ctx));
+
+        rsx_signal.cloned()
     });
 
-    if !contents.is_empty() {
-        let first = contents.remove(0);
-        let rest = contents;
-        if rest.len() > 0 {
-            use_after_render(move || {
-                let dom = VirtualDom::new_with_props(
-                    RecursiveComponent,
-                    RecursiveComponentProps {
-                        contents: rest.to_vec(),
-                    },
-                );
-                dioxus::desktop::window().new_window(dom, Default::default());
-            });
-        }
-
-        rsx! {
-            div { {format!("{:?}",first)} }
-        }
-    } else {
-        rsx! {
-            div { "Tried to render a component with no contents" }
-        }
-    }
+    launch_virtual_dom_blocking(virtual_dom, config);
 }
